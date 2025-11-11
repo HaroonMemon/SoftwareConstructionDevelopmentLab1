30894ec261ecefad0dd3174aa1b2774d35c2ba2d
HaroonMemon <memonharoon839@gmail.com>
2025-11-11 16:41:25 +0500

    Resolved merge conflict

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Welcome to Git Branching and Merging Lab!")
 print("This is a new feature added in the feature-message branch.")
-print("This line is from the conflict-demo branch.")
+print("This line is from the main branch.")
+print("This line combines both branches successfully.")
